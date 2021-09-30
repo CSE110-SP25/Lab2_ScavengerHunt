7bc1229a53c9bfb6f4783b3896051e7cdec615c0
CamdynR <rasque.camdyn@gmail.com>
2021-09-30 15:35:41 -0700

    First content commit

---
+++ dontlookhere.js
@@ -0,0 +1,8 @@
+// Question 6 Passcode: ThisIsJS
+console.log('Question 7 Passcode: YouFoundMe');
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+  fetch('./question10.jpg');
+});
